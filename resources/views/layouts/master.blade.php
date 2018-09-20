fa05681a870dea108342938ffe01247c1dbdfdbe
veronj <veron_j@etna-alternance.net>
2018-09-20 16:49:48 +0200

    first product and style

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,13 +6,18 @@
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+<link rel="stylesheet" href="{{ URL::to('css/mycss.css') }}">
+
 @yield('styles')
 </head>
 <body>
 
 @include('partials.header')
 
+<div class="container">
 @yield('content')
+</div>
+
 
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
