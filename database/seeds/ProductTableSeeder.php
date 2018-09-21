38131a3764e19877b7af00a79d1f760158f45409
veronj <veron_j@etna-alternance.net>
2018-09-21 14:59:11 +0200

    products seed

---
+++ database/seeds/ProductTableSeeder.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProductTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $product = new \App\Product([
+            'imagePath' => 'https://images.gr-assets.com/books/1389929874l/140671.jpg',
+            'title' => 'The Black Company',
+            'description' => 'Follow the Black Company destiny',
+            'price' => 12
+        ]);
+        $product->save();
+
+        $product = new \App\Product([
+            'imagePath' => 'https://images-na.ssl-images-amazon.com/images/I/511RvCsDsTL._SX322_BO1,204,203,200_.jpg',
+            'title' => 'The Black Company',
+            'description' => 'Follow the Black Company destiny',
+            'price' => 12
+        ]);
+        $product->save();
+
+        $product = new \App\Product([
+            'imagePath' => 'https://images.gr-assets.com/books/1389929874l/140671.jpg',
+            'title' => 'The Black Company',
+            'description' => 'Follow the Black Company destiny',
+            'price' => 12
+        ]);
+        $product->save();
+
+        $product = new \App\Product([
+            'imagePath' => 'https://images-na.ssl-images-amazon.com/images/I/511RvCsDsTL._SX322_BO1,204,203,200_.jpg',
+            'title' => 'The Black Company',
+            'description' => 'Follow the Black Company destiny',
+            'price' => 12
+        ]);
+        $product->save();
+
+        $product = new \App\Product([
+            'imagePath' => 'https://images.gr-assets.com/books/1389929874l/140671.jpg',
+            'title' => 'The Black Company',
+            'description' => 'Follow the Black Company destiny',
+            'price' => 12
+        ]);
+        $product->save();
+
+        $product = new \App\Product([
+            'imagePath' => 'https://images-na.ssl-images-amazon.com/images/I/511RvCsDsTL._SX322_BO1,204,203,200_.jpg',
+            'title' => 'The Black Company',
+            'description' => 'Follow the Black Company destiny',
+            'price' => 12
+        ]);
+        $product->save();
+    }
+}
--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -14,5 +14,72 @@
       </div>
     </div>
   </div>
+
+  <div class="col-sm-6 col-md-4">
+    <div class="mythumbnail">
+      <img src="https://images.gr-assets.com/books/1389929874l/140671.jpg" alt="..." class="img-responsive">
+      <div class="caption">
+        <h3>Black company</h3>
+        <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima ratione, rem ad labore nulla nemo. Impedit doloremque excepturi voluptates asperiores dolorum hic accusamus. Cum ipsam sapiente soluta iste, asperiores officia.</p>
+        <div class="clearfix">
+        <div class="pull-left price">12€</div>
+         <a href="#" class="btn btn-primary pull-right" role="button">Add to cart</a></div>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-sm-6 col-md-4">
+    <div class="mythumbnail">
+      <img src="https://images.gr-assets.com/books/1389929874l/140671.jpg" alt="..." class="img-responsive">
+      <div class="caption">
+        <h3>Black company</h3>
+        <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima ratione, rem ad labore nulla nemo. Impedit doloremque excepturi voluptates asperiores dolorum hic accusamus. Cum ipsam sapiente soluta iste, asperiores officia.</p>
+        <div class="clearfix">
+        <div class="pull-left price">12€</div>
+         <a href="#" class="btn btn-primary pull-right" role="button">Add to cart</a></div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="mythumbnail">
+      <img src="https://images.gr-assets.com/books/1389929874l/140671.jpg" alt="..." class="img-responsive">
+      <div class="caption">
+        <h3>Black company</h3>
+        <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima ratione, rem ad labore nulla nemo. Impedit doloremque excepturi voluptates asperiores dolorum hic accusamus. Cum ipsam sapiente soluta iste, asperiores officia.</p>
+        <div class="clearfix">
+        <div class="pull-left price">12€</div>
+         <a href="#" class="btn btn-primary pull-right" role="button">Add to cart</a></div>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-sm-6 col-md-4">
+    <div class="mythumbnail">
+      <img src="https://images.gr-assets.com/books/1389929874l/140671.jpg" alt="..." class="img-responsive">
+      <div class="caption">
+        <h3>Black company</h3>
+        <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima ratione, rem ad labore nulla nemo. Impedit doloremque excepturi voluptates asperiores dolorum hic accusamus. Cum ipsam sapiente soluta iste, asperiores officia.</p>
+        <div class="clearfix">
+        <div class="pull-left price">12€</div>
+         <a href="#" class="btn btn-primary pull-right" role="button">Add to cart</a></div>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-sm-6 col-md-4">
+    <div class="mythumbnail">
+      <img src="https://images.gr-assets.com/books/1389929874l/140671.jpg" alt="..." class="img-responsive">
+      <div class="caption">
+        <h3>Black company</h3>
+        <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima ratione, rem ad labore nulla nemo. Impedit doloremque excepturi voluptates asperiores dolorum hic accusamus. Cum ipsam sapiente soluta iste, asperiores officia.</p>
+        <div class="clearfix">
+        <div class="pull-left price">12€</div>
+         <a href="#" class="btn btn-primary pull-right" role="button">Add to cart</a></div>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
