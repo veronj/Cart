8f698123205de9d5e422a85b2b7d9649b58f5cd2
veronj <veron_j@etna-alternance.net>
2018-09-21 14:43:29 +0200

    price

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -7,8 +7,10 @@
       <img src="https://images.gr-assets.com/books/1389929874l/140671.jpg" alt="..." class="img-responsive">
       <div class="caption">
         <h3>Black company</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima ratione, rem ad labore nulla nemo. Impedit doloremque excepturi voluptates asperiores dolorum hic accusamus. Cum ipsam sapiente soluta iste, asperiores officia.</p>
-        <div class="clearfix"> <a href="#" class="btn btn-primary pull-right" role="button">Button</a></div>
+        <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima ratione, rem ad labore nulla nemo. Impedit doloremque excepturi voluptates asperiores dolorum hic accusamus. Cum ipsam sapiente soluta iste, asperiores officia.</p>
+        <div class="clearfix">
+        <div class="pull-left price">12€</div>
+         <a href="#" class="btn btn-primary pull-right" role="button">Add to cart</a></div>
       </div>
     </div>
   </div>
