168b1f0ae668bbeeb331c988130d5c1b1ac9a414
veronj <veron_j@etna-alternance.net>
2018-09-28 20:15:02 +0200

    cart view

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,7 +2,7 @@
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">My site</a>
+      <a class="navbar-brand" href="{{ route('product.index') }}">My site</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
--- resources/views/shop/shopping-cart.blade.php
+++ resources/views/shop/shopping-cart.blade.php
@@ -4,19 +4,39 @@
 <h1>Shopping Cart</h1>
   @if(Session::has('cart'))
   <div class="row">
-  <div class="col-sm-6 col-md-4">
+  <div class="col-sm-6 col-md-6 col-md-offset-3 col-sm-offset-3">
   <ul class="list-group">
   @foreach($products as $product)
   <li class="list-group-item">
   <span class="badge">{{ $product['qty'] }}</span>
   <strong>{{ $product['item']['title'] }}</strong>
-  <span class="label label-success">{{ $product['price'] }}</span>
+  <span class="label label-success">{{ $product['price'] }} €</span>
+  <div class="btn-group">
+  <button type="button" class="btn btn-primary btn-xs dropdown-toggle" data-toggle="dropdown">Action<span class="caret"></span></button>
+  <ul class="dropdown-menu">
+  <li><a href="#">Suppr 1</a></li>
+  <li><a href="#">Suppr all</a></li>
+  </ul>
+  </div>
   </li>
   @endforeach
   
   </ul>
   </div>
   </div>
+
+  <div class="row">
+  <div class="col-sm-6 col-md-6 col-md-offset-3 col-sm-offset-3">
+  <strong>Total : {{ $totalPrice }}</strong>
+  </div>
+  </div>
+<hr>
+<div class="row">
+<div class="col-sm-6 col-md-6 col-md-offset-3 col-sm-offset-3">
+  <button type="button" class="btn btn-success">Check Out</button>
+  </div>
+  </div>
+
   @else
   @endif
  
