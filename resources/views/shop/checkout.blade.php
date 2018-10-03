8fe4d4a4f106841e1f66d2f206a35aea89e10ebe
veronj <veron_j@etna-alternance.net>
2018-10-03 16:51:50 +0200

    clean up

--- resources/views/shop/checkout.blade.php
+++ resources/views/shop/checkout.blade.php
@@ -7,12 +7,9 @@
   <div class="col-sm-6 col-md-4 col-md-offset-4 col-sm-offset-3">
   <h4>Your total: {{ $total }}</h4>
   <form action="{{ route('checkout') }}" method="post">
-  
+
   </form>
   </div>
   </div>
 
-  @else
-  @endif
- 
 @endsection
