86533e04e7b2c7c0e7daed30387522def4112ad8
veronj <veron_j@etna-alternance.net>
2018-10-19 00:09:09 +0200

    syntax

--- resources/views/user/signin.blade.php
+++ resources/views/user/signin.blade.php
@@ -4,7 +4,7 @@
 <div class="row">
 <div class="col-md-4 col-md-offset-4">
     <h1>Sign In</h1>
-    @if (count($errors) > 0)
+    @if ($errors->any())
     <div class="alert alert-danger">
         @foreach($errors->all() as $error)
         <p>{{ $error }}</p>
